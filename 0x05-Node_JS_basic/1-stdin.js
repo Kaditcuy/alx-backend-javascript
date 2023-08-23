2fcd9262d817344a5dd7fcf866afb2a6db3287a0
Kaditcuy <philipukchimaobi@gmail.com>
2023-08-23 05:11:23 +0100

    Using Process stdin

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,13 @@
+process.stdin.setEncoding('utf8');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+	const userName = process.stdin.read();
+	if (userName !== null) {
+		process.stdout.write(`Your name is: ${userName.toString()}`);
+	}
+});
+
+process.stdin.on('end', () => {
+	process.stdout.write("This important software is now closing\n");
+});

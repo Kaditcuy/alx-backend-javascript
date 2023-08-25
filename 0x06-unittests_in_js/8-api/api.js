913069bd32041c89ef85408a826909341d733cbc
Kaditcuy <philipukchimaobi@gmail.com>
2023-08-25 06:01:04 +0100

    api.js

---
+++ 0x06-unittests_in_js/8-api/api.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+Port = 7865;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.listen(Port, () => {
+  console.log(`API available on localhost port ${Port}`);
+});
+
